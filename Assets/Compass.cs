fd82e0fdf640ebf51c11bd8f9cc1c2e063d03438
Ruben Frosali <ruben@pxlartificer.com>
2019-09-06 12:50:43 +0900

    c1

--- Assets/Compass.cs
+++ Assets/Compass.cs
@@ -7,20 +7,18 @@ public class Compass : MonoBehaviour
 {
 
     RawImage rawImage;
-    public float smoothTime = 0.3F;
-    private Vector3 velocity = Vector3.zero;
+    public float shiftX = 0.01F;
 
-    // Start is called before the first frame update
     void Start()
     {
         rawImage = GetComponent<RawImage>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
-        //rawImage.uvRect = new Rect(x,0,1,1) ;
-        Debug.Log(Vector3.SignedAngle(Camera.main.transform.forward,Vector3.forward,Vector3.up));
+        float direction= (Camera.main.transform.eulerAngles.y / 360f);
+        rawImage.uvRect = new Rect(shiftX+direction, 0, 0.5f, 1);
+
     }
 }
